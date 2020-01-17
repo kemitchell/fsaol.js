7225dfbdd300ca59bc8ad0286d91da3c78641118
Kyle E. Mitchell <kyle@kemitchell.com>
2020-01-16 23:40:30 -0800

    Test .stream({ start })

--- test.js
+++ test.js
@@ -24,11 +24,11 @@ tape('init, write, stream, read, head', (test) => {
     (done) => aol.initialize(done),
     (done) => aol.write(entry, done),
     (done) => {
-      var entries = []
+      var streamed = []
       aol.stream()
-        .on('data', (entry) => { entries.push(entry) })
+        .on('data', (entry) => { streamed.push(entry) })
         .once('end', () => {
-          test.deepEqual(entries[0], entry, 'stream')
+          test.deepEqual(streamed[0], entry, 'stream')
           done()
         })
     },
@@ -45,6 +45,39 @@ tape('init, write, stream, read, head', (test) => {
         test.equal(head, 1, 'head')
         done()
       })
+    }
+  ], (error) => {
+    test.ifError(error, 'no error')
+    test.end()
+    rimraf.sync(directory)
+  })
+})
+
+tape('init, write multiple, stream with offset', (test) => {
+  var directory, aol
+  var entries = [{ x: 1 }, { y: 2 }, { z: 3 }]
+  runSeries([
+    (done) => {
+      fs.mkdtemp(path.join(os.tmpdir(), 'fsaol-'), (error, tmp) => {
+        if (error) return done(error)
+        directory = tmp
+        aol = new FSAOL({ hashFunction, encoding, directory })
+        done()
+      })
+    },
+    (done) => aol.initialize(done),
+    (done) => runSeries(
+      entries.map((entry) => (done) => { aol.write(entry, done) }),
+      done
+    ),
+    (done) => {
+      var streamed = []
+      aol.stream({ start: 1 })
+        .on('data', (entry) => { streamed.push(entry) })
+        .once('end', () => {
+          test.deepEqual(streamed, entries.slice(1), 'streams')
+          done()
+        })
     }
   ], (error) => {
     test.ifError(error, 'no error')
